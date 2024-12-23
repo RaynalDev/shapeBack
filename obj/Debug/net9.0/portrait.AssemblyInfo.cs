d7b50ac1504c343d9ac4b3616d907fb2dbd2a35d
Gold Smith <gholdsmith@gmail.com>
2024-12-23 11:01:14 +0100

    commit to merge

--- obj/Debug/net9.0/portrait.AssemblyInfo.cs
+++ obj/Debug/net9.0/portrait.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portrait")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53cb375909467d070738dc605a63cfbb3bf6da85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290baab6635aa406f495f1c95db8a97356e92f42")]
 [assembly: System.Reflection.AssemblyProductAttribute("portrait")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portrait")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
